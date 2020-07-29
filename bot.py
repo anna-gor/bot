2c53208bd86d9c603d74ae4bf6852b1b43a41d47
anna-gor <Sadzho@gmail.com>
2020-07-29 18:05:07 +0300

    horoscope

--- bot.py
+++ bot.py
@@ -1,71 +1,129 @@
-import requests
-import datetime
+# Подключаем модуль случайных чисел
 
-class BotHandler:
+import random
 
-    def __init__(self, token):
-        self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+# Подключаем модуль для Телеграма
 
-    def get_updates(self, offset=None, timeout=30):
-        method = 'getUpdates'
-        params = {'timeout': timeout, 'offset': offset}
-        resp = requests.get(self.api_url + method, params)
-        result_json = resp.json()['result']
-        return result_json
+import telebot
 
-    def send_message(self, chat_id, text):
-        params = {'chat_id': chat_id, 'text': text}
-        method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params)
-        return resp
+# Указываем токен
 
-    def get_last_update(self):
-        get_result = self.get_updates()
+bot = telebot.TeleBot('1320536909:AAFmjzIQDxsVVGHUoO5h7neWtha2CM_ZINo')
 
-        if len(get_result) > 0:
-            last_update = get_result[-1]
-        else:
-            last_update = get_result[len(get_result)]
+# Импортируем типы из модуля, чтобы создавать кнопки
 
-        return last_update
+from telebot import types
 
-greet_bot = BotHandler('1320536909:AAFmjzIQDxsVVGHUoO5h7neWtha2CM_ZINo')
-greetings = ('здравствуй', 'привет', 'ку', 'здорово')
-now = datetime.datetime.now()
+# Заготовки для трёх предложений
 
+first = ["Сегодня — идеальный день для новых начинаний.","Оптимальный день для того, чтобы решиться на смелый поступок!","Будьте осторожны, сегодня звёзды могут повлиять на ваше финансовое состояние.","Лучшее время для того, чтобы начать новые отношения или разобраться со старыми.","Плодотворный день для того, чтобы разобраться с накопившимися делами."]
 
-def main():
-    new_offset = None
-    today = now.day
-    hour = now.hour
+second = ["Но помните, что даже в этом случае нужно не забывать про","Если поедете за город, заранее подумайте про","Те, кто сегодня нацелен выполнить множество дел, должны помнить про","Если у вас упадок сил, обратите внимание на","Помните, что мысли материальны, а значит вам в течение дня нужно постоянно думать про"]
 
-    while True:
-        greet_bot.get_updates(new_offset)
+second_add = ["отношения с друзьями и близкими.","работу и деловые вопросы, которые могут так некстати помешать планам.","себя и своё здоровье, иначе к вечеру возможен полный раздрай.","бытовые вопросы — особенно те, которые вы не доделали вчера.","отдых, чтобы не превратить себя в загнанную лошадь в конце месяца."]
 
-        last_update = greet_bot.get_last_update()
+third = ["Злые языки могут говорить вам обратное, но сегодня их слушать не нужно.","Знайте, что успех благоволит только настойчивым, поэтому посвятите этот день воспитанию духа.","Даже если вы не сможете уменьшить влияние ретроградного Меркурия, то хотя бы доведите дела до конца.","Не нужно бояться одиноких встреч — сегодня то самое время, когда они значат многое.","Если встретите незнакомца на пути — проявите участие, и тогда эта встреча посулит вам приятные хлопоты."]
 
-        last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
+# Метод, который получает сообщения и обрабатывает их
 
-        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
-            today += 1
+@bot.message_handler(content_types=['text'])
 
-        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-            today += 1
+def get_text_messages(message):
 
-        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-            today += 1
+    # Если написали «Привет»
 
-        new_offset = last_update_id + 1
+    if message.text == "Привет":
 
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        exit()
+        # Пишем приветствие
+
+        bot.send_message(message.from_user.id, "Привет, сейчас я расскажу тебе гороскоп на сегодня.")
+
+        # Готовим кнопки
+
+        keyboard = types.InlineKeyboardMarkup()
+
+        # По очереди готовим текст и обработчик для каждого знака зодиака
+
+        key_oven = types.InlineKeyboardButton(text='Овен', callback_data='zodiac')
+
+        # И добавляем кнопку на экран
+
+        keyboard.add(key_oven)
+
+        key_telec = types.InlineKeyboardButton(text='Телец', callback_data='zodiac')
+
+        keyboard.add(key_telec)
+
+        key_bliznecy = types.InlineKeyboardButton(text='Близнецы', callback_data='zodiac')
+
+        keyboard.add(key_bliznecy)
+
+        key_rak = types.InlineKeyboardButton(text='Рак', callback_data='zodiac')
+
+        keyboard.add(key_rak)
+
+        key_lev = types.InlineKeyboardButton(text='Лев', callback_data='zodiac')
+
+        keyboard.add(key_lev)
+
+        key_deva = types.InlineKeyboardButton(text='Дева', callback_data='zodiac')
+
+        keyboard.add(key_deva)
+
+        key_vesy = types.InlineKeyboardButton(text='Весы', callback_data='zodiac')
+
+        keyboard.add(key_vesy)
+
+        key_scorpion = types.InlineKeyboardButton(text='Скорпион', callback_data='zodiac')
+
+        keyboard.add(key_scorpion)
+
+        key_strelec = types.InlineKeyboardButton(text='Стрелец', callback_data='zodiac')
+
+        keyboard.add(key_strelec)
+
+        key_kozerog = types.InlineKeyboardButton(text='Козерог', callback_data='zodiac')
+
+        keyboard.add(key_kozerog)
+
+        key_vodoley = types.InlineKeyboardButton(text='Водолей', callback_data='zodiac')
+
+        keyboard.add(key_vodoley)
+
+        key_ryby = types.InlineKeyboardButton(text='Рыбы', callback_data='zodiac')
+
+        keyboard.add(key_ryby)
+
+        # Показываем все кнопки сразу и пишем сообщение о выборе
+
+        bot.send_message(message.from_user.id, text='Выбери свой знак зодиака', reply_markup=keyboard)
+
+    elif message.text == "/help":
+
+        bot.send_message(message.from_user.id, "Напиши Привет")
+
+    else:
+
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+# Обработчик нажатий на кнопки
+
+@bot.callback_query_handler(func=lambda call: True)
+
+def callback_worker(call):
+
+    # Если нажали на одну из 12 кнопок — выводим гороскоп
+
+    if call.data == "zodiac":
+
+        # Формируем гороскоп
+
+        msg = random.choice(first) + ' ' + random.choice(second) + ' ' + random.choice(second_add) + ' ' + random.choice(third)
+
+        # Отправляем текст в Телеграм
+
+        bot.send_message(call.message.chat.id, msg)
+
+# Запускаем постоянный опрос бота в Телеграме
+
+bot.polling(none_stop=True, interval=0)
